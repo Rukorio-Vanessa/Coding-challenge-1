ded5169479c547e9fd5b74986758221019efcbbf
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-28 21:58:46 +0300

    complete code challenge

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Coding-challenge-1
+
+### Author : Vanessa Rukorio 28/11/12
+
+## Project Description
+### Code Challenges 
+* Challenge 1: Student Grade Generator (Toy Problem)
+  Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+  A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+* Challenge 2: Speed Detector (Toy Problem)
+  Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+  For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+
+ 
+
+* Challenge 3: Net Salary Calculator (Toy Problem)
+  Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
+
+## Code challenge Solutions
+* Challenge 1:
+  - npm install prompt-sync: run on the terminal in order to be able to us the prompt command to prompt the user to enter the grade.
+  function gradingOfResults(gradePercentage){
+  while (gradePercentage >= 0 && gradePercentage <= 100){}
+  } //returns a grade A, B, C, D OR E depending on the grade percentage input(between 0 &100)
+
+* Challenge 2: 
+   - function speedLimit(speed){}
+   //the `speedlimit` returns 'ok' if speed is less than 70
+   - print("Points:"+ ((speed-70)/5));
+   //the speed limit is greater than 70
+   prints 'License suspended' if points exceed 12
+
+* Challenge 3:
+   -  function monthlyGrossSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances){}
+   // `monthlyGrossSalary` returns the value of the gross salary with input value of basic salary and benefits and allowances
+   -  function monthlyPayeRate(monthlyBasicSalary){}
+   `monthlyPayeRate` returns the PAYE tax on the basic salary
+   -  const monthlyNhifDeduction = (monthlyGrossSalary) => {} 
+   `monthlyNhifDeduction` returns value of NHIF deductions on the grossmonthlysalary given certain conditions
+   -  const monthlyNssfDeductions = 200
+   `monthlyNssfDeductions` returns the value of 200/= as the nssf deduction
+   -  function monthlyNetSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances){}
+   `monthlyNetSalary` returns the Net Salary given the monthlyBasicSalary and monthlyBenefitsAndAllowances, and by calling the previously defined functions
+   Net Salary = monthlyGrossSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances) - monthlyPayeRate(monthlyBasicSalary) - monthlyNhifDeduction(monthlyGrossSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances)) - monthlyNssfDeductions
+
+### Requirements
+* Text editor eg [Visual Studio Code](https://code.visualstudio.com/download)
+* [Testing Platform](Platform URL)
+
+
+
+## Dependencies
+- package.json
+- package-lock.json
+
+## Technologies Used
+1. Javascript
+
+## Contact Information
+* Email : vanessa.kirangari@moringaschool.com
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//Qs.1
+//to prompt a user to enter a number between 0 and 100
 const prompt=require("prompt-sync")({sigint:true})
-prompt("enter a number")
+prompt("enter a number between 0 and 100: ")
+//a function for grading results as:(A, B, C, D, E) depending on the score/percentage
+//the vallues need to be between 0 and 100 for a function to run and output a grade
 function gradingOfResults(gradePercentage){
 while (gradePercentage >= 0 && gradePercentage <= 100){
 if (gradePercentage >= 79)
@@ -34,13 +38,18 @@ if (gradePercentage >= 79)
     return `Grade: ${grade}`
 } 
 }   
+//confirming function works as intended
 console.log(gradingOfResults(90))
 
-//qs.2
-function print(tag, value){
-    console.info(tag, value)
+//Qs.2
+
+function print(value){
+    console.info(value)
 } 
-       
+//input speed of a car
+//speed limit is 70-print 'ok'
+//if speed limit is passed,for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+//if  more than 12 points, the function should print: “License suspended”.
 function speedLimit(speed){
     if(speed < 70){
     print('OK')
@@ -54,4 +63,105 @@ function speedLimit(speed){
     print('License suspended')
     }
 }
-console.log(speedLimit(90))
+//confirming function works
+print (speedLimit(90))
+
+//qs.3
+//***Goal is to calcute the monthly net salary-final salary after benefits are added, and taxes and deductables removed
+//monthly gross salary given basic salary and benefits/asllowances
+function monthlyGrossSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances){
+    monthlyGross = monthlyBasicSalary + monthlyBenefitsAndAllowances
+    return monthlyGross
+}
+//confirming gross salary function works!
+console.log(monthlyGrossSalary(100000, 40000))
+
+//calculating the monthly PAYE(tax) while taking into account the basic salary
+function monthlyPayeRate(monthlyBasicSalary){
+    if (monthlyBasicSalary > 32333){
+        monthlyPaye = monthlyBasicSalary * 30/100
+        return monthlyPaye
+    }
+    else{
+        if(monthlyBasicSalary >=24001 && monthlyBasicSalary <=32333){
+            monthlyPaye = monthlyBasicSalary * 25/100
+            return monthlyPaye
+        }
+        else{
+            monthlyPaye = monthlyBasicSalary * 20/100
+            return monthlyPaye
+        }
+    }
+}
+//confirming my PAYE function gives the correct output given the conditions
+console.log(monthlyPayeRate(100000))
+
+//giving the monthly NHIF deductions based on the anount og gross salary earned 
+const monthlyNhifDeduction = (monthlyGrossSalary) => {
+    if (monthlyGrossSalary <= 5999) {
+        return 150
+    }
+    else if (monthlyGrossSalary <= 7999) {
+        return 300
+    }
+    else if (monthlyGrossSalary <= 11999) {
+        return 400
+    }
+    else if (monthlyGrossSalary <= 14999) {
+        return 500
+    }
+    else if (monthlyGrossSalary <= 19999) {
+        return 600
+    }
+    else if (monthlyGrossSalary <= 24999) {
+        return 750
+    }
+    else if (monthlyGrossSalary <= 29999) {
+        return 850
+    }
+    else if (monthlyGrossSalary <= 34999) {
+        return 900
+    }
+    else if (monthlyGrossSalary <= 39999) {
+        return 950
+    }
+    else if (monthlyGrossSalary <= 44999) {
+        return 1000
+    }
+    else if (monthlyGrossSalary <= 49999) {
+        return 1100
+    }
+    else if (monthlyGrossSalary <= 59999) {
+        return 1200
+    }
+    else if (monthlyGrossSalary <= 69999) {
+        return 1300
+    }
+    else if (monthlyGrossSalary <= 79999) {
+        return 1400
+    }
+    else if (monthlyGrossSalary <= 89999) {
+        return 1500
+    }
+    else if (monthlyGrossSalary <= 99999) {
+        return 1600
+    }
+    else {
+        return 1700
+    }
+}
+//confirming the code returns the correct output
+console.log(monthlyNhifDeduction(monthlyGrossSalary(100000, 40000)))
+
+//the monthly NSSF deductions is a constant 200/=
+const monthlyNssfDeductions = 200
+
+//Finally calculating the net salary by calling the already defined functions
+//i.e Net Salary= Gross salary - paye - all other deductions
+function monthlyNetSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances){
+    netSalary = monthlyGrossSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances) - monthlyPayeRate(monthlyBasicSalary) - monthlyNhifDeduction(monthlyGrossSalary(monthlyBasicSalary, monthlyBenefitsAndAllowances)) - monthlyNssfDeductions
+    return `Net Salary: ${netSalary}`
+}
+//confirming I can get the net salary by inputing th basic salary and benefits
+console.log(monthlyNetSalary(100000, 40000))
+
